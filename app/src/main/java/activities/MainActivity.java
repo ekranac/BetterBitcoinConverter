00fdd093154b8ada2f73c3fe8157cc40b962c178
ekranac <besal.ziga@gmail.com>
2015-09-18 18:39:17 +0200

    Added dropdown for currencies

--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -1,17 +1,22 @@
 package activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
 import com.ziga.bitcoinconverter.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import helpers.APIHelper;
+import models.JSONResponse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +32,16 @@ public class MainActivity extends AppCompatActivity {
         // Show keyboard on start
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
 
-        APIHelper.getApiData();
+        APIHelper.getApiData(this);
+
+        SharedPreferences preferences = getSharedPreferences(APIHelper.PREFERENCES_KEY, Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String conversions = preferences.getString(APIHelper.CONVERSIONS_KEY, "");
+        if(!conversions.equals("")) // If there's data from before (only not present when the user opens the app for the first time and isn't connected to the network)
+        {
+            JSONResponse response = gson.fromJson(conversions, JSONResponse.class);
+            input.setText(response.USD.last_five_minutes.toString() + response.USD.symbol);
+        }
 
     }
 
@@ -45,11 +59,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/helpers/APIHelper.java
+++ app/src/main/java/helpers/APIHelper.java
@@ -1,6 +1,11 @@
 package helpers;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
+
+import com.google.gson.Gson;
 
 import interfaces.APIInterface;
 import models.JSONResponse;
@@ -12,8 +17,10 @@ import retrofit.client.Response;
 public class APIHelper
 {
     public static final String BASE_URL = "https://blockchain.info";
+    public static final String PREFERENCES_KEY = "com.ziga.bitcoinconverter.preferences";
+    public static final String CONVERSIONS_KEY = "com.ziga.bitcoinconverter.preferences.conversions";
 
-    public static void getApiData()
+    public static void getApiData(final Activity activity)
     {
         RestAdapter adapter = new RestAdapter.Builder()
                 .setEndpoint(BASE_URL)
@@ -24,12 +31,26 @@ public class APIHelper
             @Override
             public void success(JSONResponse jsonResponse, Response response) {
                 Log.i("USD", jsonResponse.USD.last_five_minutes.toString());
+                // TODO save to SharedPreferences
+                saveConversionsData(activity, jsonResponse);
             }
 
             @Override
             public void failure(RetrofitError error) {
-
+                Log.e("ERROR", error.toString());
             }
         });
+    }
+
+
+    public static void saveConversionsData(Activity activity, JSONResponse jsonResponse)
+    {
+        SharedPreferences preferences = activity.getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = preferences.edit();
+        Gson gson = new Gson();
+        String response = gson.toJson(jsonResponse);
+        editor.putString(CONVERSIONS_KEY, response);
+        editor.commit();
     }
 }
